db2df7ca5475b1a09995fba334f3123d23a92d9b
Niels <Niels@NIELS-PC>
2022-10-27 09:36:01 +0200

    Add project files.

---
+++ FotoBoxer/Program.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+internal class Program
+{
+    public static Dictionary<DateTime, string> Log = new Dictionary<DateTime, string>();
+
+    private static async Task Main(string[] args)
+    {
+        if (!args.Any())
+        {
+            Console.WriteLine("No arguments were passed");
+            return;
+        }
+
+        var dir = args[0];
+        var editDir = $"{dir.UpDir()}\\boxed";
+
+        string[] images = new string[0];
+        images = Directory.GetFiles(dir, "*.jpg");
+
+        try
+        {
+            Directory.CreateDirectory(editDir);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"[{DateTime.Now}] - {ex.Message}\n");
+            Log.Add(DateTime.Now, ex.ToString());
+            Console.ReadKey();
+
+            return;
+        }
+
+        foreach (var image in images)
+        {
+            try
+            {
+                Console.Write($"Processing {image}... \r");
+                await Task.Run(() => MakeBoxedEdit(image, editDir.CreateFileName(image)));
+                Log.Add(DateTime.Now, $"Processed {image}\n");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"[{DateTime.Now}] - {ex.Message}\n");
+                Log.Add(DateTime.Now, ex.ToString());
+            }
+        }
+
+        Console.WriteLine("\n\nDone processing images!");
+
+        File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\log", JsonConvert.SerializeObject(Log));
+        Console.ReadKey();
+    }
+
+    private static void MakeBoxedEdit(string fileName, string newFileName)
+    {
+        var origBitmap = (Bitmap)Image.FromFile(fileName);              // Open stream to original photo
+        var corrRect = origBitmap.GetCorrectRect();                     // Get a rectangle with even width/height ratio
+
+        var newBitmap = new Bitmap(corrRect.Width, corrRect.Height);    // Create new bitmap with dimensions from corrected rectangle
+        var graphics = Graphics.FromImage(newBitmap);                   // Create graphics object with new bitmap
+        var centRect = origBitmap.GetCenteredRect(corrRect);            // Create rectangle where the bounds of the original photo are centered
+
+        graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+        graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+        graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+        graphics.FillRectangle(Brushes.White, corrRect);                // Fill corrected rectangle with solid background White
+        graphics.DrawImage(origBitmap, centRect);                       // Draw photo to newly created bitmap
+
+        newBitmap.Save(newFileName);                                    // Save new bitmap
+    }
+
+    public static bool Check(object value)
+    {
+        return value == null;
+    }
+}
+
+public static class StringExtensions
+{
+    public static string UpDir(this string dir)
+    {
+        var parts = dir.Split('\\');
+        var builder = new StringBuilder();
+
+        Array.ForEach(parts[0..(parts.Length - 1)],
+                      a => builder.Append($"{a}\\"));
+
+        return builder.ToString();
+    }
+
+    public static bool IsDirNameCorrect(this string dir)
+    {
+        var invalidDirChars = Path.GetInvalidPathChars();
+
+        Array.ForEach(dir.ToCharArray(), chr =>
+        {
+            if (invalidDirChars.Any(ichr => ichr == chr))
+                return;
+        });
+
+        return false;
+    }
+
+    public static string CreateFileName(this string newDir, string fileName)
+    {
+        return $"{newDir}\\{Path.GetFileName(fileName)}";
+    }
+}
+
+public static class BitmapExtensions
+{
+    public static Rectangle GetCenteredRect(this Bitmap bitmap, Rectangle correctedRect)
+    {
+        return new Rectangle((correctedRect.Width / 2) - (bitmap.Width / 2), (correctedRect.Height / 2) - (bitmap.Height / 2), bitmap.Width, bitmap.Height);
+    }
+
+    public static Rectangle GetCorrectRect(this Bitmap bitmap)
+    {
+        if (bitmap.Width > bitmap.Height)
+        {
+            return new Rectangle(0, 0, bitmap.Width, bitmap.Width);
+        }
+        else
+        {
+            return new Rectangle(0, 0, bitmap.Height, bitmap.Height);
+        }
+    }
+}
---
+++ ImageEditor/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("FotoEditor ~>");
+
+            var command = Console.ReadLine();
+            var parts = command?.Split("\\s").ParseArgs();
+
+            if (parts == null)
+            {
+                Console.WriteLine("Het opgegeven commando was op een incorrecte wijze ingevuld. Controleer a.u.b. of er geen extra spaties in staan.");
+                return;
+            }
+
+            switch (parts.Command)
+            {
+                case "box":
+                    var input = parts.Parameters?["input"]
+                        .CheckNullOrEmpty();
+                    var output = parts.Parameters?["output"]
+                        .CheckNullOrEmpty();
+                    var barColor = parts.Parameters?["barColor"]
+                        .CheckNullOrEmpty();
+
+                    // gecheckt voor parameters
+                    // toevoegen code voor het 'inboxen' van fotos die ongelijke groottes/breedtes hebben
+
+                    break;
+
+                case "resize": break;
+                case "changebg": break;
+                case "changefg": break;
+                case "copy": break;
+                case "rename": break;
+            }
+        }
+    }
+}
+
+public static class ConsoleExtensions
+{
+    public static CommandArgs ParseArgs(this string[] args)
+    {
+        var commandArgs = new CommandArgs(args[0]);
+
+        Array.ForEach(args[1..args.Length], a =>
+        {
+            if (a.Contains('='))
+            {
+                var split = a.Split('=');
+                commandArgs.Parameters?.Add(split[0], split[1]);
+            }
+            if (!a.Contains('='))
+            {
+                commandArgs.Switches?.Add(a);
+            }
+        });
+
+        return commandArgs;
+    }
+
+    public static KeyValuePair<string, object> CheckNullOrEmpty(this KeyValuePair<string, object> parameter)
+    {
+        if (parameter.Key == null || parameter.Value == null)
+            throw new KeyNotFoundException(nameof(parameter));
+
+        if (parameter.Key != null && parameter.Key != string.Empty)
+            if (parameter.Value != null)
+                return parameter;
+
+        throw new KeyNotFoundException(nameof(parameter));
+    }
+}
+
+public class CommandArgs
+{
+    public string? Command { get; set; }
+    public ParameterDict<string, object>? Parameters { get; set; }
+    public List<string>? Switches { get; set; }
+
+    public CommandArgs(string command)
+    {
+        Command = command;
+        Parameters = new ParameterDict<string, object>();
+        Switches = new List<string>();
+    }
+}
+
+public class ParameterDict<TKey, TValue> : Dictionary<TKey, TValue> where TKey : IEquatable<TKey>
+{
+    public KeyValuePair<TKey, TValue> this[int index]
+    {
+        get
+        {
+            if (index < 0 || index > Count)
+                throw new IndexOutOfRangeException(nameof(index));
+
+            var key = Keys.ToArray()[index];
+            var value = Values.ToArray()[index];
+
+            return new KeyValuePair<TKey, TValue>(key, value);
+        }
+    }
+
+    public KeyValuePair<TKey, TValue> this[string key]
+    {
+        get
+        {
+            return this[key];
+        }
+    }
+}
